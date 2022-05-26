b8cbd5d18aad9a3690c5e538ed42132d6191dd9d
Marten Sova <marten@Martens-MacBook-Pro.local>
2022-05-25 18:27:20 -0700

    server allows multiple inputs.

--- server.py
+++ server.py
@@ -6,11 +6,31 @@ import sys
 
 host = ""
 port = 6697
+queue = 5 #temp value
+size = 1024 #temp value
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-s.listen(1)
-while(1):
-    conn, addr = s.accept()
-    data = conn.recv(20)
-    conn.send(data)
-    conn.close()
+s.listen(queue)
+input = [s, sys.stdin]
+running = 1
+while(running):
+    inputready, outputready, exceptready = select.select(input, [], [])
+    
+    for x in inputready:
+        if x == s:
+            client, addr = s.accept()
+            input.append(client)
+
+        elif x == sys.stdin:
+            temp = sys.stdin.readline()
+            running = 0
+
+        else:
+            data = x.recv(size)
+            if data:
+                x.send(data)
+            else:
+                x.close()
+                input.remove(x)
+
+s.close()
